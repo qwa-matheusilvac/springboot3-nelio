265bf129730cb90c47254323b2e1a6e0a3eb21a5
qwa-matheusilvac <matheus.correia@qwasolucoes.com.br>
2025-04-02 11:22:10 -0300

    feat: user controller

--- src/main/java/br/com/qwasolucoes/cursoudemy/curso_spring/web/controller/UserController.java
+++ src/main/java/br/com/qwasolucoes/cursoudemy/curso_spring/web/controller/UserController.java
@@ -1,15 +1,18 @@
 package br.com.qwasolucoes.cursoudemy.curso_spring.web.controller;
 
 
+import br.com.qwasolucoes.cursoudemy.curso_spring.domain.dto.UserReqDto;
 import br.com.qwasolucoes.cursoudemy.curso_spring.domain.persistence.entity.User;
 import br.com.qwasolucoes.cursoudemy.curso_spring.domain.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping("api/users")
@@ -21,6 +24,13 @@ public class UserController {
     @GetMapping
     public Page<User> obterTodos(@PageableDefault(size = 10) Pageable page){
         return userService.obterTodos(page);
+    }
+
+    @PostMapping
+    public ResponseEntity<User> criarNovo(@RequestBody UserReqDto userReqDto, UriComponentsBuilder uriBuilder){
+        User user = new User(userReqDto);
+        URI uri = uriBuilder.path("/books/{id}").buildAndExpand(user.getId()).toUri();
+        return ResponseEntity.created(uri).body(user);
     }
 
 }
